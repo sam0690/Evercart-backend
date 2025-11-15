a4f981104716f28d1a357a46a2b3763fd6c9392d
Samriddha Jung Thapa <idk123sam@gmail.com>
2025-11-16 00:04:10 +0545

    some fixes

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 import os
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
